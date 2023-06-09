57461e74ea30776487a843610de43c03fd85c331
Web-Tim <teichmanntim@outlook.de>
2023-06-09 23:38:09 +0200

    updates

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -8,7 +8,13 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       if not opts.ensure_installed then opts.ensure_installed = {} end
       require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "clangd",
+        "cmake",
+        "jdtls",
+        "texlab",
+        "marksman",
+        "rust_analyzer",
       })
     end,
   },
@@ -20,8 +26,8 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       if not opts.ensure_installed then opts.ensure_installed = {} end
       require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "stylua",
       })
     end,
   },
@@ -32,7 +38,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       if not opts.ensure_installed then opts.ensure_installed = {} end
       require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "python",
       })
     end,
   },
